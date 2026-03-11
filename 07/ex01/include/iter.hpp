4c15a81b1a155d2c17607bbf327c7440c5447182
vbonnard <vbonnard.dev@gmail.com>
2026-03-11 14:35:52 +0100

    fix une connerie de con de 42

--- 07/ex01/include/iter.hpp
+++ 07/ex01/include/iter.hpp
@@ -14,14 +14,3 @@ void iter(const T* array, size_t size, F function){
   for (size_t i = 0; i < size; i++)
     function(array[i]);
 }
-
-
-template <typename T>
-void print(const T& any){
-  std::cout << any << std::endl;
-}
-
-template <typename T>
-void incr(T& any){
-  ++any;
-}
--- 07/ex01/src/main.cpp
+++ 07/ex01/src/main.cpp
@@ -1,5 +1,16 @@
 #include "../include/iter.hpp"
 
+template <typename T>
+void print(const T& any){
+  std::cout << any << std::endl;
+}
+
+template <typename T>
+void incr(T& any){
+  ++any;
+}
+
+
 int main() {
     int array[] = {0, 1, 2, 3};
     iter(array, 4, print<int>);
